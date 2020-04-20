2706179ae6ede053ae590ccd633cf5074711bd61
Cristina Laryea <crispad06@gmail.com>
2020-04-20 00:09:29 -0400

    tribonacci challenge

--- code wars/challenges.py
+++ code wars/challenges.py
@@ -1,4 +1,4 @@
-# Create fibonacci sequence with n elements and output it in a list
+# Create fibonacci sequence with n elements and output it in a list. If n  == 0 return empty list. 
 # 0 1 1 2 3 5 8 11 19 ...etc
 # [0, 1, 1, 2, 3, 5, 8, 11, 19]
 
@@ -17,4 +17,31 @@ def fib_sequence(n):
             base.append(base[counter - 1] + base[counter - 2])
             counter += 1
     return base
-print(fib_sequence(7))
+#print(fib_sequence(7))
+
+
+#2
+# Create a tribonacci sequence with n elements and output it in a list. If n  == 0 return empty list. 
+# 0 1 1 2 4 7 13 24 ... etc 
+# [0, 1, 1, 2, 4, 7, 13, 24]
+
+
+def trib_sequence(n):
+    base = [0,1,1]
+    if n == 0:
+        return []
+    if n == 1: 
+        return [base[0]]
+    if n == 2:
+        return [base[0] + base[1]]
+    if n == 3: 
+        return base
+    if n > 3:
+        counter = 3
+        while counter < n:
+            base.append(base[counter - 3] + base[counter - 2] + base[counter - 1])
+            counter += 1
+        return base
+    
+print(trib_sequence(4))
+
