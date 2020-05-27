1ad050eefade5b0e970894dee5c4cdb97309a9bb
Cristina Laryea <crispad06@gmail.com>
2020-05-27 18:33:28 -0400

    twoNumberSum

--- algo/two_number_sum.js
+++ algo/two_number_sum.js
@@ -1,3 +1,14 @@
 function twoNumberSum(array, targetSum) {
-    
+    for (let i = 0; i < array.length - 1; i++) {
+        const firstNumber = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            const secondNumber = array[j];
+            if (firstNumber + secondNumber === targetSum) {
+                return [firstNumber, secondNumber];
+            }
+        }
+    }
+    return [];
 }
+
+twoNumberSum[3, 5, -4, 8, 11, 1, -1, 6];
