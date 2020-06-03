80bf5b2b8aa5f1030846c61dafe9a2bccd9ad036
Cristina Laryea <crispad06@gmail.com>
2020-06-03 12:13:24 -0400

    change file name to challenges, condensed all coding challenges to one file

--- algo/challenges.js
+++ algo/challenges.js
@@ -1,3 +1,6 @@
+/* Two Number Sum */
+
+
 function twoNumberSum(array, targetSum) {
     for (let i = 0; i < array.length - 1; i++) {
         const firstNumber = array[i];
@@ -12,3 +15,15 @@ function twoNumberSum(array, targetSum) {
 }
 
 
+/* Nth Fibonacci */
+
+function getNthFib(n) {
+    if (n === 2) {
+        return 1;
+    }else if (n === 1) {
+        return 0;
+    }else {
+        return getNthFib(n-1) + getNthFib(n - 2);
+    }
+   }
+   
