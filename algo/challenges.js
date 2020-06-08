7de264d0642bc0b342f4e047836e1b6c7f932081
Cristina Laryea <crispad06@gmail.com>
2020-06-08 11:56:02 -0400

    nth fib

--- algo/challenges.js
+++ algo/challenges.js
@@ -65,3 +65,16 @@ function productSum(array, multiplier = 1){
     }
     return sum * multiplier;
 }
+
+
+/* Nth Fibonacci */
+
+function getNthFib(n) {
+    if (n === 2) {
+        return 1;
+    }else if (n === 1) {
+        return 0;
+    }else {
+        return getNthFib(n - 1) + getNthFib(n - 2);
+    }
+}
