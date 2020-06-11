735fc92189ad008c69a98bade81bfd3e97ea74de
Cristina Laryea <crispad06@gmail.com>
2020-06-11 11:56:02 -0400

    four sum

--- algo/challenges.js
+++ algo/challenges.js
@@ -78,3 +78,31 @@ function productSum(array, multiplier = 1){
 //         return getNthFib(n - 1) + getNthFib(n - 2);
 //     }
 // }
+
+
+/* Four Number Sum */
+
+function fourNumbersSum(array, targetSum) {
+    const allPairSums = {}
+    const quadruplets = []
+    for (let i = 1; i < array.length - 1; i++) {
+        for(let j = 1; j < array.length; j++) {
+            const currentSum = array[i] + array[j];
+            const difference = targetSum - currentSum;
+            if (difference in allPairSums) {
+                for (const pair of allPairSums[difference]) {
+                    quadruplets.push(pair.concat([array[i], array[j]]));
+                }
+            }
+        }
+        for (let k = 0; k < i; k++) {
+            const currentSum = array[i] + array[k];
+            if (!(currentSum in allPairSums)) {
+                allPairSums[currentSum] = [[array[k], array[i]]];
+            }else {
+                allPairSums[currentSum].push([array[k], array[i]]);
+            }
+        }
+    }
+    return quadruplets;
+}
