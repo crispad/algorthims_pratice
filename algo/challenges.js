de1ad4fae3fd07406da1ee9fe62c2d5de09ba2bb
Cristina Laryea <crispad06@gmail.com>
2020-06-12 16:07:51 -0400

    product sum

--- algo/challenges.js
+++ algo/challenges.js
@@ -51,3 +51,17 @@ function threeNumberSum(array, targetSum) {
    }
    return triple
 }
+
+/* Product Sum */
+
+function productSum(array, multiplier = 1){
+    let sum = 0;
+    for(const element of array) {
+        if (Array.isArray(element)) {
+            sum += productSum(element, multiplier + 1);
+        }else {
+            sum += element;
+        }
+    }
+    return sum * multiplier;
+}
