ff245aaaa3b02676536aa78b61efbdacf577ec81
Cristina Laryea <crispad06@gmail.com>
2020-06-15 18:14:21 -0400

    removed nth fib, had double

--- algo/challenges.js
+++ algo/challenges.js
@@ -67,18 +67,6 @@ function productSum(array, multiplier = 1){
 }
 
 
-// /* Nth Fibonacci */
-
-// function getNthFib(n) {
-//     if (n === 2) {
-//         return 1;
-//     }else if (n === 1) {
-//         return 0;
-//     }else {
-//         return getNthFib(n - 1) + getNthFib(n - 2);
-//     }
-// }
-
 
 /* Four Number Sum */
 
