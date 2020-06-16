f968617e53700e1965f5b0f8ae99b8481a4b410c
Cristina Laryea <crispad06@gmail.com>
2020-06-16 17:29:51 -0400

    challenge

--- algo/challenges.js
+++ algo/challenges.js
@@ -14,6 +14,25 @@ function twoNumberSum(array, targetSum) {
     return [];
 }
 
+/* Two Number Sum (2nd solution) */
+
+function twoNumberSum(array, targetSum) {
+    array.sort((a, b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]];
+        }else if (currentSum < targetSum) {
+            left++;
+        }else if (currentSum > targetSum)  {
+            right--;
+        }
+    }
+    return [];
+}
+
 
 /* Nth Fibonacci */
 
